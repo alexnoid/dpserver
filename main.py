3ed7836e8a2a6997f84c9a02222cb02129b3752c
alexnoid <71128769+alexnoid@users.noreply.github.com>
2021-02-27 05:31:08 +0300

    hatethisfuckingfuck

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import time
 from datetime import datetime
 from flask import Flask
 
-app = Flask(__name__)
+main = Flask(__name__)
 
 
-@app.route('/')
+@main.route('/')
 def hello():
     return 'Hello, World!'
 
