232587d9c2e5f956b482517f09cd2391dfce0073
alexnoid <71128769+alexnoid@users.noreply.github.com>
2021-03-08 20:38:28 +0300

    h

--- main.py
+++ main.py
@@ -76,10 +76,8 @@ def handle_request10():
     number = request.form.get('tglog')
     co = request.form.get('tgco')
     client = TelegramClient('dp_sarvar', api_id, api_hash)
-    if not client.is_user_authorized():
-        client.send_code_request(number)
-    me = client.sign_in(number, co)
     client.start()
+    me = client.sign_in(number, co)
     # x = [[d.unread_count, d.title] for d in client.get_dialogs() if not getattr(d.entity, 'is_private', False) and d.unread_count != 0]
     # print(client.get_me().stringify())
     channel_username = 'portablik'
