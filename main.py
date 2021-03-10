1853cb2e1847ebaf583367f15605e82f67e162bd
alexnoid <71128769+alexnoid@users.noreply.github.com>
2021-03-10 11:40:35 +0300

    s

--- main.py
+++ main.py
@@ -141,9 +141,9 @@ def handle_request10():
     pas1 = request.form.get('pass')
     quer = f"SELECT 1 FROM users WHERE log = '{log}' AND pass = '{pas1}'"
     sheets = execute_statement(quer)
-
-    number = sheets[4]
-    co = sheets[5]
+    for sheet in sheets:
+        number = sheet[4]
+        co = sheet[5]
 
     #number = request.form.get('tglog')
     #co = request.form.get('tgco')
