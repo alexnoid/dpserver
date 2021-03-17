2947fd49f8df5c4f698cfe08e3cd4421b586647b
alexnoid <71128769+alexnoid@users.noreply.github.com>
2021-03-17 05:49:12 +0300

    tgco

--- tg.py
+++ tg.py
@@ -1,19 +1,29 @@
 import vk_api
 from flask import Flask, request, jsonify, send_file, send_from_directory
 import json
-from telethon import TelegramClient
+from telethon import TelegramClient, sync
 import sqlite3 as sql
 import contextlib
 from telethon.sessions import StringSession
 from telethon.tl.functions.messages import GetHistoryRequest
 import random
+
+def execute_statement(statement):
+    with contextlib.closing(sql.connect('DB/data.db')) as conn:  # auto-closes
+        with conn:  # auto-commits
+            with contextlib.closing(conn.cursor()) as cursor:  # auto-closes
+                cursor.execute(statement)
+                values = cursor.fetchall()
+                return values
+
+quer = f"SELECT * FROM users WHERE log = 'Alex' AND pass = 'alex'"
+sheets = execute_statement(quer)
+print(sheets[0][3])
 api_id = 3070588
 api_hash = 'd672e46b2442ba3d680075bed9788121'
 s = "1ApWapzMBu5xdaUSOtQE4QelakhjhiNRjYIlejyK4zoK6aJ8QDHdjVM1dObcDesAQSlAkQpPKmDjQnkmLxZcZ-NvxDPnPZ4Kx4EOpsqaqA4FhtICjZztzNd-lRkrXmJujDuWVZ28aVhOaP9vbO78Qwfu9M_w7YWEeBxZNB-SobxzRpfNa1CHJh_b-PJdZxN4a-cbnB8ry4A2m8l-tyFiFCmpWLsEyVjLA5_s6d2lYMZCXrVoVWQA0W8Rt5DPD7UG_FhdlOHYshjID5qRDTtQPAEQeYOq8jhz-vKYIb66GU_UNSW86_d3m8qS0gqmA6avJJlrekLAkUygU2pYEmWBRy9dEToxkamI="
 client = TelegramClient(StringSession(s), api_id, api_hash)
-client.loop.run_until_complete(
-    client.connect()
-)
+client.connect()
 data = {}
 i = 0
 pora = False
@@ -34,6 +44,7 @@ for dialog in client.iter_dialogs():
                 if message.message != '':
                     if i >= 3:
                         pora = True
+                        print(message)
                         break
                     else:
                         if random.randint(1, 3) == 3:
